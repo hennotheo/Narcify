ea0c8477456d9e47e5e8cc5de2d2b8c2ffd782cf
NazioLT <zoupeurletroll@gmail.com>
2025-03-14 12:48:28 +0100

    :test_tube: Add result match query test

--- Tests/NarcifyCoreTest/SearchTests.cs
+++ Tests/NarcifyCoreTest/SearchTests.cs
@@ -1,6 +1,8 @@
 ﻿using NarcifyCore;
 using NarcifyCore.Interfaces;
+using NarcifyCore.Models;
 using NarcifyCore.Repository;
+using NUnit.Framework.Legacy;
 
 namespace NarcifyCoreTest;
 
@@ -13,15 +15,33 @@ public class SearchTests
 
     [Test]
     [TestCase("A")]
-    public void CallingResearchFunction(string research)
+    public void CallingResearchFunction(string query)
     {
         IRepository repository = new YoutubeRepository();
 
-        if (repository.Search("test").Count != 0)
+        if (repository.Search(query).Count != 0)
         {
             Assert.Pass();
         }
 
         Assert.Fail();
+    }
+
+    [Test]
+    [TestCase("Query")]
+    public void CheckResearchMatchQuery(string query)
+    {
+        IRepository repository = new YoutubeRepository();
+        IReadOnlyCollection<ResearchResult> results = repository.Search(query);
+
+        foreach (var result in results)
+        {
+            if (result.Query != query)
+            {
+                Assert.Fail();
+            }
+        }
+
+        Assert.Pass();
     }
 }
